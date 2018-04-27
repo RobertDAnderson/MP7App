b5eb5ff9ca82aea6f2f5bd4dad20e166f244d669
robertdanderson <rdander2@illinois.edu>
2018-04-27 10:02:11 -0500

    line 52 comment. almost ready to begin

--- MainActivity.java
+++ MainActivity.java
@@ -2,31 +2,34 @@ package edu.illinois.cs.cs125.mp7app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 //hi there
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MP7:Main";
+    public final String deckID = " ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+
     }
 
     /**
@@ -45,7 +48,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void getID() {
+        // start a new game and save the ID. almost working.... i think
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",
+                null,null,null);
+        JsonParser parser = new JsonParser();
+        JsonObject deckIDgetter = parser.parse().getAsJsonObject();
+        if (deckIDgetter.has("deck_id")) {
+            deckIDgetter = deckIDgetter.get("metadata").getAsJsonObject();
+            String deckID = deckIDgetter.toString();
+        }
+
+    }
+
     public void startWar(final View press) {
+
 
         // get https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
         // get deck_id; save as string;
@@ -74,11 +94,11 @@ public class MainActivity extends AppCompatActivity {
         // https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/pot/add/?cards=p1code,p2code
         // if p1value > p2value
         //      recurse through pot list add all cards in pot list to pile player1
-        //      https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/player1/add/?cards=XX,YY
+        //      https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/player1/add/?cards=p1code,p2code
         //      clear list
         // if p1value > p2value
         //      recurse through pot list add all cards in pot list to pile player1
-        //      https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/player2/add/?cards=XX,YY
+        //      https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/player2/add/?cards=p1code,p2code
         //      clear list
         // if p1value == p2value
         //
